023d6260e36a28699d47e9252eb2b24dff53798a
Rian Sanjaya <rian.sj@gmail.com>
2024-06-23 23:14:33 +0700

    fetch from wix api and create server component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,10 +1,64 @@
+import { wixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
 import Image from "next/image"
 import Link from "next/link"
 
-const ProductList = () => {
+const PRODUCT_PER_PAGE = 20
+
+const ProductList = async ({ 
+  categoryId, 
+  limit, 
+}: { 
+  categoryId: string; 
+  limit?: number; 
+}) => {
+  const wixClient = await wixClientServer()
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .find()
+  console.log('res: ', res.items[0].price)
+
   return (
     <div className="mt-12 flex justify-between flex-wrap gap-x-8 gap-y-16">
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+      {res.items.map((product: products.Product) => (
+        <Link 
+          href={"/" + product.slug} 
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" 
+          key={product._id}
+        >
+          <div className="relative w-full h-80">
+            <Image 
+              src={product.media?.mainMedia?.image?.url || "product.png"}
+              alt="" 
+              fill 
+              sizes="25vw" 
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+            />
+            {product.media?.items && (
+              <Image 
+                src={product.media?.items[1].image?.url || "product.png"}
+                alt="" 
+                fill 
+                sizes="25vw" 
+                className="absolute object-cover rounded-md"
+              />
+            )}
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">{product.price?.price}</span>
+          </div>
+          {product.additionalInfoSections && (
+            <div className="text-sm text-gray-500">
+              {product.additionalInfoSections?.find((section: any) => section.title === "shortDesc")?.description || ""}
+            </div>
+          )}
+          <button className="rounded-2xl ring-1 ring-badgeNumber text-badgeNumber w-max px-4 py-2 text-xs hover:bg-badgeNumber hover:text-white">Add to Cart</button>
+        </Link>
+      ))}
+      {/* <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
         <div className="relative w-full h-80">
           <Image 
             src="https://images.pexels.com/photos/25188494/pexels-photo-25188494/free-photo-of-close-up-of-a-cup-of-coffee-on-a-tray.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" 
@@ -27,8 +81,8 @@ const ProductList = () => {
         </div>
         <div className="text-sm text-gray-500">My description</div>
         <button className="rounded-2xl ring-1 ring-badgeNumber text-badgeNumber w-max px-4 py-2 text-xs hover:bg-badgeNumber hover:text-white">Add to Cart</button>
-      </Link>
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+      </Link> */}
+      {/* <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
         <div className="relative w-full h-80">
           <Image 
             src="https://images.pexels.com/photos/20318312/pexels-photo-20318312/free-photo-of-woman-with-black-hair-sitting-in-living-room.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" 
@@ -97,7 +151,7 @@ const ProductList = () => {
         </div>
         <div className="text-sm text-gray-500">My description</div>
         <button className="rounded-2xl ring-1 ring-badgeNumber text-badgeNumber w-max px-4 py-2 text-xs hover:bg-badgeNumber hover:text-white">Add to Cart</button>
-      </Link>
+      </Link> */}
     </div>
   )
 }
