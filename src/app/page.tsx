183709156b4e997e8955730eaf1c5a42a923b868
Rian Sanjaya <rian.sj@gmail.com>
2024-07-01 21:05:31 +0700

    delete a character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ const HomePage = async () => {
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
         <Suspense fallback={<Skeleton />}>
           <CategoryList />
-          \</Suspense>
+        </Suspense>
       </div>
       {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"> */}
         {/* <h1 className="text-2xl">New Products</h1> */}
