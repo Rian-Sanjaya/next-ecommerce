d0ad6e9207ff6794744703eb3b4257fd4149c231
Rian Sanjaya <rian.sj@gmail.com>
2024-07-01 16:13:01 +0700

    ignore typescript error on CartModal.tsx cart.subtotal

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -93,7 +93,7 @@ const CartModal = () => {
                     <span
                       className="text-blue-500"
                       style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
-                      onClick={() => removeItem(wixClient, item._id)}
+                      onClick={() => removeItem(wixClient, item._id!)}
                     >
                       Remove
                     </span>
@@ -106,6 +106,7 @@ const CartModal = () => {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
+              {/* @ts-ignore */}
               <span className="">${cart.subtotal.amount}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
